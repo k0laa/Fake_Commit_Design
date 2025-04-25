129e2aa4e3ca6234c3da40738c8ea33bde175d56
Buğra Yalçın <bugrayalcn1@gmail.com>
2025-04-25 20:16:44 +0300

    dizin düzeni adına tema için ayrı bir dosya oluşturuldu.

---
+++ ui/theme.py
@@ -0,0 +1,35 @@
+DEFAULT_THEME = """
+            QWidget {
+                background-color: #1c1b2f;
+                color: #e0e6f0;
+                font-family: 'Segoe UI';
+            }
+            QPushButton {
+                border-radius: 20px;
+                padding: 10px;
+                min-width: 40px;
+                min-height: 40px;
+            }
+            QPushButton#ClearButton, QPushButton#EraseButton {
+                min-width: 100px;
+                max-width: 100px;
+            }
+            QPushButton#ClearButton {
+                background-color: #8a7fd3;
+                color: white;
+            }
+            QPushButton#ClearButton:hover {
+                background-color: #a299e0;
+            }
+            QPushButton#EraseButton {
+                background-color: #444;
+                color: white;
+                border: 3px solid transparent;
+            }
+            QPushButton#EraseButton:checked {
+                border: 3px solid yellow;
+            }
+            QPushButton#EraseButton:hover {
+                background-color: #666;
+            }
+        """
